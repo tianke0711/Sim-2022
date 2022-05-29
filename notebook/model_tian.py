2cedfb4293c95df2831ac29b9020a316b3901969
tianke0711 <tianke0711@gmail.com>
2022-05-29 09:34:34 +0900

    add tian model

---
+++ notebook/model_tian.py
@@ -0,0 +1,113 @@
+from transformers import BertTokenizer, BertModel
+from transformers import DistilBertTokenizer, DistilBertModel
+from transformers import RobertaTokenizer, RobertaModel
+from torch import nn
+from torch.optim import Adam
+
+class Dataset(torch.utils.data.Dataset):
+
+    def __init__(self, df):
+        
+        self.labels = [labels[label] for label in df['label']]
+        self.texts = [tokenizer(text, 
+                               padding='max_length', max_length = 512, truncation=True,
+                                return_tensors="pt") for text in df['text']]
+
+    def classes(self):
+        return self.labels
+
+    def __len__(self):
+        return len(self.labels)
+
+    def get_batch_labels(self, idx):
+        # Fetch a batch of labels
+        return np.array(self.labels[idx])
+
+    def get_batch_texts(self, idx):
+        # Fetch a batch of inputs
+        return self.texts[idx]
+
+    def __getitem__(self, idx):
+
+        batch_texts = self.get_batch_texts(idx)
+        batch_y = self.get_batch_labels(idx)
+
+        return batch_texts, batch_y
+
+class BertClassifier_large(nn.Module):
+
+    def __init__(self, dropout=0.2):
+
+        super(BertClassifier_large, self).__init__()
+        self.bert = BertModel.from_pretrained('bert-large-cased')
+        self.dropout = nn.Dropout(dropout)
+        self.linear = nn.Linear(1024, 2)
+        self.relu = nn.ReLU()
+
+    def forward(self, input_id, mask):
+
+        _, pooled_output = self.bert(input_ids= input_id, attention_mask=mask,return_dict=False)
+        dropout_output = self.dropout(pooled_output)
+        linear_output = self.linear(dropout_output)
+        final_layer = self.relu(linear_output)
+
+        return final_layer
+    
+    
+class BertClassifier(nn.Module):
+
+    def __init__(self, dropout=0.2):
+
+        super(BertClassifier, self).__init__()
+
+        self.bert = BertModel.from_pretrained('bert-base-cased')
+        #self.bert = BertModel.from_pretrained('bert-large-cased')
+        self.dropout = nn.Dropout(dropout)
+        self.linear = nn.Linear(768, 5)
+        self.relu = nn.ReLU()
+
+    def forward(self, input_id, mask):
+
+        _, pooled_output = self.bert(input_ids= input_id, attention_mask=mask,return_dict=False)
+        dropout_output = self.dropout(pooled_output)
+        linear_output = self.linear(dropout_output)
+        final_layer = self.relu(linear_output)
+
+        return final_layer
+    
+class DistillBertClassifier(nn.Module):
+    def __init__(self, dropout=0.2):
+        super(DistillBertClassifier, self).__init__()
+        self.dbert = DistilBertModel.from_pretrained("distilbert-base-uncased")
+        self.dropout = nn.Dropout(dropout)
+        self.linear = nn.Linear(768, 5)
+        self.relu = nn.ReLU()
+        
+    def forward(self, input_id, mask):
+        pooled_output = self.dbert(input_ids=input_id, attention_mask=mask)
+        hidden_state = pooled_output[0]
+        pooled_output = hidden_state[:, 0]
+        dropout_output = self.dropout(pooled_output)
+        linear_output = self.linear(dropout_output)
+        final_layer = self.relu(linear_output)
+
+        return final_layer
+
+
+class RobertaClassifier(nn.Module):
+    def __init__(self, dropout=0.2):
+        super(RobertaClassifier, self).__init__()
+        self.roberta = RobertaModel.from_pretrained('roberta-base')
+        self.dropout = nn.Dropout(dropout)
+        self.linear = nn.Linear(768, 5)
+        self.relu = nn.ReLU()
+        
+    def forward(self, input_id, mask):
+        pooled_output = self.roberta(input_ids=input_id, attention_mask=mask)
+        hidden_state = pooled_output[0]
+        pooled_output = hidden_state[:, 0]
+        dropout_output = self.dropout(pooled_output)
+        linear_output = self.linear(dropout_output)
+        final_layer = self.relu(linear_output)
+        
+        return final_layer
